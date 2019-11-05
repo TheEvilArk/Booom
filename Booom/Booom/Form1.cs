fa97d6c62cd3d2115ae0bd0accb3f379e1b708d0
MaksSwed <46872656+TheEvilArk@users.noreply.github.com>
2019-11-05 21:25:18 +0300

    Add files via upload
    a small virus of its own design. It does not harm the computer. Very simple screamer

---
+++ Booom/Booom/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Runtime.InteropServices;
+
+namespace Booom
+{
+    public partial class Form1 : Form
+    {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+        IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Sure", "Error 000x0228xx1337x1488", MessageBoxButtons.OK);
+            Form1 Fm1 = new Form1();
+            timer1.Enabled = true;
+            for (; ;)
+            {
+
+
+                SoundPlayer player = new SoundPlayer("LowWave.wav");
+                player.Play();
+                this.Hide();
+            }
+        }
+
+        int i = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            this.Hide();
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+            (IntPtr)APPCOMMAND_VOLUME_UP);
+            if (i == 150)
+            {
+                timer1.Enabled = false;
+                i = 0;
+                Application.Exit();
+            }
+        }
+    }
+}
